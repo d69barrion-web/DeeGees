c7da2e68d8d1c6b645d18377b0eabb23c16deef5
DANIEL BARRION <d69barrion@gmail.com>
2026-05-03 21:05:39 +0800

    Remove email from premium status update
    Removed email field from user premium status update.

--- server.js
+++ server.js
@@ -99,7 +99,6 @@ const premiumUntil = new Date(now.getTime() + 30 * 24 * 60 * 60 * 1000);
 await userRef.set(
   {
     isPremium: true,
-    email: userId.email,
     paidAt: admin.firestore.FieldValue.serverTimestamp(),
     premiumUntil: premiumUntil,
     paymentProvider: "xendit"
